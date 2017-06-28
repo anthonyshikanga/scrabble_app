936be0b968c99a59381b203ecea6963407644fdf
anthonyshikanga <anthonyshikanaga@gmail.com>
2017-06-28 10:40:55 +0300

    modified app.rb

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,14 @@
 require('sinatra')
   require('sinatra/reloader')
+  require('./lib/scrabble')
+  also_reload('lib/**/*.rb')
 
   get('/') do
     erb(:index)
   end
+
+  get('/title') do
+   @title = params.fetch('scrabble').scrabble()
+   erb(:title)
+  end
+
